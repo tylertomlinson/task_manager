2c3bc168eafca9c3cbe21d7f25ce171bd5d03d9f
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-14 17:21:28 -0600

    Rails app

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,20 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+  end
+
+  def create
+    task = Task.new({
+     title: params[:task][:title],
+     description: params[:task][:description]
+     })
+
+   task.save
+
+   redirect_to '/tasks'
+  end
+end
